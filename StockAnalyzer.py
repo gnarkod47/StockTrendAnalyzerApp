575ae3ed8a19be3c04c916c015ba5c8584c55957
Gautham Narkodu <gnarkod123@gmail.com>
2022-12-18 06:54:31 +0530

    Update StockAnalyzer.py

--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -2,12 +2,12 @@
 import streamlit as st
 from datetime import date
 
-import yahoo_finance as yf
+import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
+START = "2017-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
